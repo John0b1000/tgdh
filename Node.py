7a2fc46ac94f98e6468c85c642a553e9327bdcd1
kousikr26 <kousik18@iitg.ac.in>
2021-04-01 13:02:45 +0530

    Optimizations and experiments

--- Node.py
+++ Node.py
@@ -3,14 +3,27 @@ from Crypto.Util import number
 class Node(object):
     g = 2
     p = number.getPrime(2048)
-    def __init__(self,id):
+    def __init__(self,id,tree):
         self.id = id
-        self.left = None
-        self.right = None
-        self.parent = None
+        # self.left = None
+        # self.right = None
+        # self.parent = None
         self.key = randint(1,int(Node.p-1))
         self.bKey = pow(Node.g,self.key,Node.p)
         self.isLeaf = True
+        self.tree = tree
+    def left(self):
+        return self.tree.left(self)
+    def right(self):
+        return self.tree.right(self)
+    def parent(self):
+        return self.tree.parent(self)
+    def setLeft(self,node):
+        self.tree.setLeft(self,node)
+    def setRight(self,node):
+        self.tree.setRight(self,node)
+    def setParent(self,node):
+        self.tree.setParent(self,node)
     def copyDataFrom(self,source, resetSource = True):
         self.key = source.key
         self.bKey = source.bKey
@@ -19,31 +32,39 @@ class Node(object):
             source.bKey = 0
     def recalculateKeyPath(self):
         curNode = self
-        while(curNode is not None):
+        while(1):
             if(not curNode.isLeaf):
-                curNode.key = pow(curNode.right.bKey,curNode.left.key,Node.p)
+                curNode.key = pow(curNode.right().bKey,curNode.left().key,Node.p)
                 curNode.bKey = pow(Node.g,curNode.key,Node.p)
-            curNode = curNode.parent
+            
+            parNode = curNode.parent()
+            if(parNode==curNode):
+                return 
+            else:
+                curNode=parNode
     def verifyIntegrity(self):
         if self.isLeaf :
             
-            if (self.left is None and self.right is None and self.verifyNodeIntegrity()):
+            if (self.left() is None and self.right() is None and self.verifyNodeIntegrity()):
                 return True
             else:
+                print("Broken leaf node in tree at id =  ",self.id)
                 return False
         else:
             
-            if (self.left.verifyIntegrity() and self.right.verifyIntegrity()) and self.verifyNodeIntegrity() and (self.left is not None and self.right is not None):
+            if (self.left().verifyIntegrity() and self.right().verifyIntegrity()) and self.verifyNodeIntegrity() and (self.left() is not None and self.right() is not None):
                 return True
+            print("Broken tree at id =  ",self.id)
             return False
     def verifyNodeIntegrity(self):
         if self.bKey == pow(Node.g,self.key,Node.p):
             return True
+        print("Broken node at id = ",self.id)
         return False
     def recursivePrint(self):
         """Returns list of strings, width, height, and horizontal coordinate of the root."""
         # No child.
-        if self.right is None and self.left is None:
+        if self.right() is None and self.left() is None:
             line = '%s' % self.id
             width = len(line)
             height = 1
@@ -51,8 +72,8 @@ class Node(object):
             return [line], width, height, middle
 
         # Only left child.
-        if self.right is None:
-            lines, n, p, x = self.left.recursivePrint()
+        if self.right() is None :
+            lines, n, p, x = self.left().recursivePrint()
             s = '%s' % self.id
             u = len(s)
             firstLine = (x + 1) * ' ' + (n - x - 1) * '_' + s
@@ -61,8 +82,8 @@ class Node(object):
             return [firstLine, secondLine] + shifted_lines, n + u, p + 2, n + u // 2
 
         # Only right child.
-        if self.left is None:
-            lines, n, p, x = self.right.recursivePrint()
+        if self.left() is None :
+            lines, n, p, x = self.right().recursivePrint()
             s = '%s' % self.id
             u = len(s)
             firstLine = s + x * '_' + (n - x) * ' '
@@ -71,8 +92,8 @@ class Node(object):
             return [firstLine, secondLine] + shifted_lines, n + u, p + 2, u // 2
 
         # Two children.
-        left, n, p, x = self.left.recursivePrint()
-        right, m, q, y = self.right.recursivePrint()
+        left, n, p, x = self.left().recursivePrint()
+        right, m, q, y = self.right().recursivePrint()
         s = '%s' % self.id
         u = len(s)
         firstLine = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
--- tree.py
+++ tree.py
@@ -16,39 +16,86 @@ from Node import Node
 import time
 from collections import deque
 import matplotlib.pyplot as plt
+import pickle
 class Tree(object):
-    def __init__(self):
+    def __init__(self,maxSize = 10000):
         
-        self.root = Node(0)
+        self.root = Node(0,self)
+        
+        self.array = [self.root] + list([None]*(2*maxSize+10))
+        
         self.nodeCount = 1
         self.queue = deque()
         self.queue.append(self.root)
+
+    def left(self,node):
+        x =  self.array[2*node.id + 1]
+        return x
+    def right(self,node):
+        x =  self.array[2*node.id + 2]
+        return x
+    def parent(self,node):
+        if(node.id==0):
+            x =  node
+        else:
+            x =  self.array[((node.id)-1)//2]
+        return x 
+    def setLeft(self,x,target):
+        self.array[2*x.id + 1] = target
+    def setRight(self,x,target):
+        self.array[2*x.id + 2] = target
+    def setParent(self,x,target):
+        if(x.id==0):
+            return 
+        self.array[(x.id-1)//2] = target
+    def setLeaf(self,x):
+        
+        self.setLeft(x,None)
+        self.setRight(x,None)
+
+        x.isLeaf=True
     def findSponsor(self):
         front = self.queue[0]
-        if(front.left is None and front.right is None):
+        if(front.left() is None and front.right() is None):
             self.queue.popleft()
             return front
         else:
             print("Node deformed")
+            return None
     def insertNewUser(self):
         sponsor = self.findSponsor()
         leftNewNode = self.createNode()
         self.queue.append(leftNewNode)
         rightNewNode = self.createNode()
         self.queue.append(rightNewNode)
-        leftNewNode.parent = sponsor
-        rightNewNode.parent = sponsor
+        leftNewNode.setParent(sponsor)
+        rightNewNode.setParent(sponsor)
         leftNewNode.copyDataFrom(sponsor)
         sponsor.isLeaf = False
         
-        sponsor.left = leftNewNode
-        sponsor.right = rightNewNode
+        sponsor.setLeft(leftNewNode)
+        sponsor.setRight(rightNewNode)
         sponsor.recalculateKeyPath()
-    def removeUser(self):
+    def deleteNewUser(self):
+        toDelete = self.queue[-1]
+        
+        if(toDelete.parent().left().id==toDelete.id):
+            toDelete.parent().copyDataFrom(toDelete.parent().right())
+        
+        else:
+            toDelete.parent().copyDataFrom(toDelete.parent().left())
+        self.setLeaf(toDelete.parent())
+        toDelete.parent().recalculateKeyPath()
+        
+        self.queue.pop()
+        self.queue.pop()
+        self.queue.appendleft(toDelete.parent())
+        
+    def removeUser(self,id):
         pass
     
     def createNode(self):
-        newNode = Node(self.nodeCount)
+        newNode = Node(self.nodeCount,self)
         self.nodeCount+=1
         return newNode
     
@@ -63,19 +110,47 @@ class Tree(object):
         return output
 
 
-NUM_NODES=1000 
+NUM_NODES=10000 
 tree = Tree()
 y = []
+x = []
 for i in range(NUM_NODES):
     start = time.time()
     tree.insertNewUser()
     end = time.time()
-    y.append(end - start)
-x= list(range(NUM_NODES))
-
+    if(i%50==0):
+        print(i)
+        y.append(end - start)
+        x.append(i)
+print(tree.verifyTreeIntegrity())
+data=[x,y]
+file=open("insertion_time","wb")
+pickle.dump(data,file)
+file.close()
 plt.plot(x,y,"o-")
 plt.xlabel("Number of users")
 plt.ylabel("Time for insertion(sec)")
 plt.savefig("./insertion_time_plot.png")
+plt.clf()
+x=[]
+y=[]
+for i in range(NUM_NODES):
+    start = time.time()
+    tree.deleteNewUser()
+    end = time.time()
+    if(i%50==0):
+        print(i)
+        y.append(end - start)
+        x.append(NUM_NODES-(i+1))
+    
+print(tree.verifyTreeIntegrity())
+data=[x,y]
+file=open("deletion_time","wb")
+pickle.dump(data,file)
+file.close()
+plt.plot(x,y,"o-")
+plt.xlabel("Number of users")
+plt.ylabel("Time for deletion(sec)")
+plt.savefig("./deletion_time_plot.png")
 plt.show()
-# print(tree.verifyTreeIntegrity())
+
